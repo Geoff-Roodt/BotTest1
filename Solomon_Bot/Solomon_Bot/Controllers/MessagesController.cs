31565cac1aec609d12ca1a5700fe00d8e7f1d23b
Geoff Roodt <geoffr@ezitracker.co.nz>
2017-06-09 11:23:11 +1200

    Initial commit - added projects and basic code to greet user

---
+++ Solomon_Bot/Solomon_Bot/Controllers/MessagesController.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Autofac;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Connector;
+using Activity = Microsoft.Bot.Connector.Activity;
+
+namespace Solomon_Bot.Controllers
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+            }
+            else if (activity.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+                var update = (IConversationUpdateActivity) activity;
+                using (var scope = DialogModule.BeginLifetimeScope(Conversation.Container, activity))
+                {
+                    var client = scope.Resolve<IConnectorClient>();
+                    if (update.MembersAdded.Any(x => x.Id != activity.Recipient.Id))
+                    {
+                        var newMembers = update.MembersAdded.Where(x => x.Id != activity.Recipient.Id).ToList();
+                        foreach (var newMember in newMembers)
+                        {
+                            var reply = activity.CreateReply($"Welcome {newMember.Name}... My name is Solomon!");
+                            await client.Conversations.ReplyToActivityAsync(reply);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
